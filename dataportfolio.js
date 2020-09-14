90498911ef4355666fdd9c4125f41843dc07b1cf
FranklinSaad <franklinpinheirosaad@hotmail.com>
2020-09-13 21:19:01 -0300

    First commit

---
+++ dataportfolio.js
@@ -0,0 +1,62 @@
+module.exports = [
+    {
+        id: "/burger.png",
+        title: "Triplo bancon burger",
+        chef: "por Jorge Relato"
+    },
+    {
+        id: "/pizza.png",
+        title: "izza 4 estações",
+        chef: "por Fabiana Melo"
+    },
+    {
+        id: "/espaguete.png",
+        title: "Espaguete ao alho",
+        chef: "por Júlia Kinoto"
+    },
+    {
+        id: "/lasanha.png",
+        title: "Lasanha mac n' cheese",
+        chef: "por Juliano Vieir"
+    },
+    {
+        id: "/doce.png",
+        title: "Docinhos pão-do-céu",
+        chef: "por Ricardo Golvea"
+    },
+    {
+        id: "/asinhas.png",
+        title: "Asinhas de frango ao barbecu",
+        chef: "por Vania Steroski"
+    },
+    {
+        id: "/burger.png",
+        title: "Triplo bancon burger",
+        chef: "por Jorge Relato"
+    },
+    {
+        id: "/pizza.png",
+        title: "izza 4 estações",
+        chef: "por Fabiana Melo"
+    },
+    {
+        id: "/espaguete.png",
+        title: "Espaguete ao alho",
+        chef: "por Júlia Kinoto"
+    },
+    {
+        id: "/lasanha.png",
+        title: "Lasanha mac n' cheese",
+        chef: "por Juliano Vieir"
+    },
+    {
+        id: "/doce.png",
+        title: "Docinhos pão-do-céu",
+        chef: "por Ricardo Golvea"
+    },
+    {
+        id: "/asinhas.png",
+        title: "Asinhas de frango ao barbecu",
+        chef: "por Vania Steroski"
+    },
+]
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+const foods = require("./dataportfolio")
+const recipes = require("./datarecipe")
+
+server.use(express.static('public'))
+
+server.use(express.static('assets'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express: server
+})
+
+server.get("/", function(req, res) {
+    return res.render("init")
+})
+
+server.get("/portfolio", function(req, res) {
+    return res.render("portfolio", { items: foods }) 
+})
+
+server.get("/about", function(req, res) {
+    return res.render("about")
+})
+
+server.get("/recipe", function(req, res) {
+    const id = req.query.id
+
+    const recipe = recipes.find(function(recipe){
+        if (recipe.id == id) {
+            return true
+        }
+    })
+
+    if (!recipe) {
+        return res.send("Not found")
+    }
+
+    return res.render("recipe", { item: recipe }) 
+})
+
+server.listen(5000, function() {
+    console.log("server is runing")
+})
